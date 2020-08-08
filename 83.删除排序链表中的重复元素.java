490b3c05ce7db7537bb790a3e1f6c911e1a72e09
long-xw <2991197007@qq.com>
2020-08-08 09:59:21 +0800

    add problem 83

---
+++ 83.删除排序链表中的重复元素.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=java
+ *
+ * [83] 删除排序链表中的重复元素
+ *
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (50.83%)
+ * Likes:    369
+ * Dislikes: 0
+ * Total Accepted:    127.2K
+ * Total Submissions: 248.8K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1->1->2
+ * 输出: 1->2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 1->1->2->3->3
+ * 输出: 1->2->3
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
+ */
+class Solution {
+  public ListNode deleteDuplicates(ListNode head) {
+
+    ListNode current = head;
+    while (current != null && current.next != null) {
+      if (current.val == current.next.val) {
+        current.next = current.next.next;
+      } else {
+        current = current.next;
+      }
+    }
+    return head;
+  }
+}
+// @lc code=end
